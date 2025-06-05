e0d3b4cea501a050e9dfbd01a6c05fd52c5804ec
eliot delemos <eliot.delemos@gmail.com>
2025-06-05 19:15:43 +0200

    connexion au back

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      // Tout ce qui commence par /api sera redirigé vers http://localhost:5148
+      "/api": {
+        target: "http://localhost:5148",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/api")
+      }
+    }
+    } 
 })
